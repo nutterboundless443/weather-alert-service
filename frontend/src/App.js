552a2bc7727cc28193fe212f34d12c7163d728a5
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-02 11:35:06 -0500

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>天气预警服务</h1>
+                <p>监测实时天气和接收预警通知。</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
